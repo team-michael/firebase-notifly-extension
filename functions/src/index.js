5c095c802b2204e121edef7dd34f633b091e1834
minyonglee <rickylee87@gmail.com>
2023-03-17 10:58:56 +0900

    Change function names and refactor

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,68 +1,14 @@
-/**
- * Triggered by a Firebase Mobile Analytics log event.
- *
- * @param {!Object} event Event payload.
- * @param {!Object} context Metadata for the event.
- */
-
-
 require("dotenv").config();
-const fetch = require("node-fetch");
 const functions = require("firebase-functions");
 const config = require("./config");
-// TODO: read conversion event lists from config
-
-/**
- * log event using notifly server-api.
- *
- * @param {!Object} eventName event_name
- * @param {!Object} userID user_id
- */
-async function trackEvent(eventName, userID) {
-  const authorizeHeaders = {
-    "Content-Type": "application/json",
-  };
-
-  const authorizeUrl = "https://api.notifly.tech/authorize";
-  const authorizeBody = {
-    userName: process.env.NOTIFLY_USERNAME,
-    password: process.env.NOTIFLY_PASSWORD,
-  };
-  const trackEventUrl = "https://api.notifly.tech/track-event";
-  const trackEventBody = {
-    projectID: process.env.NOTIFLY_PROJECT_ID,
-    eventName: eventName,
-    isGlobalEvent: false,
-    segmentationEventParamKeys: [],
-    userID: userID,
-  };
-  const authorizeEncodedBody = JSON.stringify(authorizeBody);
-  const trackEventEncodedBody = JSON.stringify(trackEventBody);
-  const token = await fetch(authorizeUrl, {
-    method: "POST",
-    headers: authorizeHeaders,
-    body: authorizeEncodedBody,
-  }).then(async (response) => {
-    const token = (await response.json());
-    return token.data;
-  });
-  const trackEventHeaders = {
-    "Content-Type": "application/json",
-    "Authorization": token,
-  };
-
-  return (await fetch(trackEventUrl, {
-    method: "POST",
-    headers: trackEventHeaders,
-    body: trackEventEncodedBody,
-  })).json();
-}
+const {sendEvent} = require("./utils");
 
 const cloudFunctions = {};
 const googleAnalyticsConversionEvents = config.googleAnalyticsConversionEvents;
+console.log(googleAnalyticsConversionEvents);
 for (const conversionEvent of googleAnalyticsConversionEvents) {
-  cloudFunctions[`firebaseToNotifly_${conversionEvent}`] = functions.analytics.event(conversionEvent).onLog(async (event) => {
-    await trackEvent(event.name, event.user?.userId);
+  cloudFunctions[conversionEvent] = functions.analytics.event(conversionEvent).onLog(async (event) => {
+    await sendEvent(event.name, event.user?.userId);
   });
 }
 
--- functions/src/utils.js
+++ functions/src/utils.js
@@ -1,7 +1,61 @@
-const addOne = (value) => {
-  return value + 1;
-};
+require("dotenv").config();
+const fetch = require("node-fetch");
+
+/**
+ * Send firebase event to Notifly.
+ *
+ * @param {!Object} eventName Name of the event
+ * @param {!Object} userID User ID
+ */
+async function sendEvent(eventName, userID) {
+  const authorizeHeaders = {
+    "Content-Type": "application/json",
+  };
+
+  const authorizeUrl = "https://api.notifly.tech/authorize";
+  const authorizeBody = {
+    userName: process.env.NOTIFLY_USERNAME,
+    password: process.env.NOTIFLY_PASSWORD,
+  };
+  const trackEventUrl = "https://api.notifly.tech/track-event";
+  const trackEventBody = {
+    projectID: process.env.NOTIFLY_PROJECT_ID,
+    eventName: eventName,
+    isGlobalEvent: false,
+    segmentationEventParamKeys: [],
+    userID: userID,
+  };
+  const authorizeEncodedBody = JSON.stringify(authorizeBody);
+  const trackEventEncodedBody = JSON.stringify(trackEventBody);
+
+  try {
+    const response = await fetch(authorizeUrl, {
+      method: "POST",
+      headers: authorizeHeaders,
+      body: authorizeEncodedBody,
+    });
+
+    const tokenData = await response.json();
+    const token = tokenData.data;
+
+    const trackEventHeaders = {
+      "Content-Type": "application/json",
+      "Authorization": token,
+    };
+
+    const trackEventResponse = await fetch(trackEventUrl, {
+      method: "POST",
+      headers: trackEventHeaders,
+      body: trackEventEncodedBody,
+    });
+
+    return await trackEventResponse.json();
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+}
 
 module.exports = {
-  addOne,
+  sendEvent,
 };
